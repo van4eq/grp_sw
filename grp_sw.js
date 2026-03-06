a6d870fc401d35fa5f397e647e4b4e885916a81b
van4eq <van4eq@mail.ru>
2026-03-06 20:54:53 +0500

    Update grp_sw.js

--- grp_sw.js
+++ grp_sw.js
@@ -65,7 +65,7 @@ $(document).on('paste',function(e){
 				stories=1;
 				let sendData=eval('({'+data.match(/\'id\'\: \d{6}\,/)+data.split(/\'id\'\: \d{6}\,/)[1].split("'params': params")[0]+'})');
 
-				until=data.split('saleUntilDate&quot;:&quot;')[1].split('T23:59:59+')[0].match(/(\d{4}-\d{2}-\d{2})/g);console.log(until[0])
+				until=data.split('saleUntilDate&quot;:&quot;')[1].split('T23:59:59+')[0].match(/(\d{4}-\d{2}-\d{2})/g)[0];
 				if(until!=null){
 					until=new Date(until).toLocaleDateString('ru-RU',options);
 				}
