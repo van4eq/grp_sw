8dc252569dfbe6acefd5094526f0e32d25c503b1
van4eq <van4eq@mail.ru>
2026-07-21 17:03:41 +0500

    Update grp_sw.js

--- grp_sw.js
+++ grp_sw.js
@@ -36,13 +36,14 @@ function weeksMonths(type='month'){
 	}else if(type=='month'){
 		startDate=new Date(now.getFullYear(),now.getMonth(),1);
 		endDate=new Date(now.getFullYear(),now.getMonth()+1,0);
-	}else if(type=='sinceUntil'){
-		startDate=since;
-		endDate=until;
-		var from=(startDate.getDate()=='2')?'со':'с';
-		var to=(startDate.getMonth()==endDate.getMonth())?'по':getMonthName(startDate)+' по';
-		return `${from} ${startDate.getDate()} ${to} ${endDate.getDate()} ${getMonthName(endDate)}`;
 	}
+//	else if(type=='sinceUntil'){ // Эта настройка на случай, если бы указывались даты начала и конца акции
+//		startDate=since;
+//		endDate=until;
+//		var from=(startDate.getDate()=='2')?'со':'с';
+//		var to=(startDate.getMonth()==endDate.getMonth())?'по':getMonthName(startDate)+' по';
+//		return `${from} ${startDate.getDate()} ${to} ${endDate.getDate()} ${getMonthName(endDate)}`;
+//	}
 	if((type=='week'||type=='month')&&startDate.getDate()>endDate.getDate()){
 		return `${startDate.getDate()} ${getMonthName(startDate)}—${endDate.getDate()} ${getMonthName(endDate)}`;
 	}else{
@@ -73,17 +74,17 @@ $(document).on('paste',function(e){
 				stories=1;
 				let sendData=eval('({'+data.match(/\'id\'\: \d{6}\,/)+data.split(/\'id\'\: \d{6}\,/)[1].split("'params': params")[0]+'})');
 
+				//try{ // Это настройка на случай, если бы указывалась дата начала акции
+				//	since=data.split('{&quot;start&quot;:&quot;')[1].split('T')[0].match(/(\d{4}-\d{2}-\d{2})/g)[0];
+				//}catch{}
+				//if(since!=null){
+				//	since=new Date(since);
+				//}
 				try{
-					since=data.split('{&quot;start&quot;:&quot;')[1].split('T')[0].match(/(\d{4}-\d{2}-\d{2})/g)[0];
-				}catch{}
-				if(since!=null){
-					since=new Date(since) //.toLocaleDateString('ru-RU',options);
-				}
-				try{
-					until=data.split('finish&quot;:&quot;')[1].split('T23:59:59')[0].match(/(\d{4}-\d{2}-\d{2})/g)[0];
+					until=data.split('&quot;finish&quot;:&quot;')[1].split('T23:59:59')[0].match(/(\d{4}-\d{2}-\d{2})/g)[0];
 				}catch{}
 				if(until!=null){
-					until=new Date(until) //.toLocaleDateString('ru-RU',options);
+					until=new Date(until);
 				}
 
 				$('#copy button').prop('disabled',false);
@@ -158,10 +159,10 @@ $(document).on('paste',function(e){
 					}else{
 						before='until';
 						if(until!=null){
-							$('#until+label').text('Акция до '+until.toLocaleDateString('ru-RU',options));
-							if(since!=null){
-								$('#until+label').text('Акция '+weeksMonths('sinceUntil'));
-							}
+							$('#until+label').text('Акция по '+until.toLocaleDateString('ru-RU',options));
+							//if(since!=null){ // Это настройка на случай, если бы указывалась дата начала акции
+							//	$('#until+label').text('Акция '+weeksMonths('sinceUntil'));
+							//}
 						}else{
 							$('#until+label').text('Акция');
 						}
